2e4134ccd3eb04c373d5663642eaedb2ee496990
Sighi-04 <filipposighinolfi@protonmail.com>
2021-07-16 23:29:23 +0200

    Created alert

--- mainscript.js
+++ mainscript.js
@@ -1,7 +1,5 @@
 async function getRoba(){
     const response=await fetch("https://api.github.com/users/Sighi-04/repos")
     const result = await response.json()
-    await result.array.forEach(element => {
-        console.log(element)
-    });
+    alert("Il primo repository è "+result[0].name)
 }
