0d92999e25e1acd836fb4da92c5d00258fa4664d
Sighi-04 <filipposighinolfi@protonmail.com>
2021-07-16 22:56:37 +0200

    Fetching my repositories using the GitHub API

--- mainscript.js
+++ mainscript.js
@@ -1,3 +1,4 @@
-function scroll(){
-    Window.scroll();
+async function getRoba(){
+    const response=await fetch("https://api.github.com/users/Sighi-04/repos")
+    const result = await response.json()
 }
