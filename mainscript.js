716b61b4e8ff7b4e947977b29604f80aa239b82a
Sighi-04 <filipposighinolfi@protonmail.com>
2021-07-16 23:21:46 +0200

    prova

--- mainscript.js
+++ mainscript.js
@@ -1,4 +1,7 @@
 async function getRoba(){
     const response=await fetch("https://api.github.com/users/Sighi-04/repos")
     const result = await response.json()
+    await result.array.forEach(element => {
+        console.log(element)
+    });
 }
