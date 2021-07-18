0b143a6e263215a568c1edcd0cdfbdce480acfa4
Sighi-04 <filipposighinolfi@protonmail.com>
2021-07-18 09:29:40 +0200

    Removed garbage text and button, added GitHub and Mail buttons

--- mainscript.js
+++ mainscript.js
@@ -1,5 +1,13 @@
 async function getRoba(){
-    const response=await fetch("https://api.github.com/users/Sighi-04/repos")
-    const result = await response.json()
-    alert("Il primo repository è "+result[0].name)
+    try {
+        const response=await fetch("https://api.github.com/users/Sighi-04/repos")
+        const result = await response.json()
+        console.log("I tuoi respositoy sono:")
+        await result.forEach(element => {
+            console.log(element.name)
+        });
+    }
+    catch{
+        alert("Impossibile ottenere i progetti recenti")
+    }
 }
