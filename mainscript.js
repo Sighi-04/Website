611b4b5727c7768c7d521c2b8c04224da2b2e9bd
Sighi-04 <filipposighinolfi@protonmail.com>
2021-07-18 22:45:57 +0200

    f

--- mainscript.js
+++ mainscript.js
@@ -6,8 +6,17 @@ async function getData(){
         await result.forEach(element => {
             console.log(element.name)
         });
+        await console.log(result)
+        changeData(result[0].name, result[1].name)
     }
     catch{
         alert("Impossibile ottenere i progetti recenti")
+    }
+}
+function changeData(proj1, proj2){
+    const pj1=document.getElementById("proj1")
+    const pj2=document.getElementById("proj2")
+    pj1.innerHTML=proj1
+    pj2.innerHTML={
     }
 }
