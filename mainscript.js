e5cf005cf84cf47a0028fbb219d29afb8c2ebe49
Sighi-04 <filipposighinolfi@protonmail.com>
2021-08-08 13:04:26 +0200

    misc

--- mainscript.js
+++ mainscript.js
@@ -1,23 +1,41 @@
-async function getData(){
+async function getReposInfo(){
     try {
         //Fetches the list of repositories, sorting by last updated
-        const response=await fetch("https://api.github.com/users/Sighi-04/repos?sort=updated")
+        let response=await fetch("https://api.github.com/users/Sighi-04/repos?sort=updated")
         //Renders the response body in json format
-        const result = await response.json()
-        //Logs to console every repository's name
-        console.log("I tuoi respositoy sono:")
-        await result.forEach(element => {
-            console.log(element.name)
-        })
-        //Fetches the commits for the latest 
+        let result = await response.json()
+        //Fetches the commits for the latest projects
+        getCommitsInfo(result[0], result[1])
     }
     catch{
         alert("Impossibile ottenere i progetti recenti")
     }
 }
-function changeData(proj1, proj2){
-    const pj1=document.getElementById("proj1")
-    const pj2=document.getElementById("proj2")
-    pj1.innerHTML=proj1
-    pj2.innerHTML=proj2
+
+async function getCommitsInfo(proj1, proj2){
+    //Defines the 2 projects as objects, which will be used to change the html
+    project1 = {
+        "name": proj1.name,
+        "description": proj1.description,
+        "committedAt": null,
+        "commitMessage": null
+    }
+    project2 = {
+        "name": proj2.name,
+        "description": proj2.description,
+        "committedAt": null,
+        "commitMessage": null
+    }
+    try {
+        //Fetches the first projects
+        let url="https://api.github.com/repos/Sighi-04/"+proj1.name+"/commits/main"
+        let response=await fetch(url)
+        console.log(url)
+        let result = await response.json()
+        await console.log(result.commit.message)
+    }
+    catch {
+        alert("Impossibile ottenere i dati sui commit")
+    }
+
 }
