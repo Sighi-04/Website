f89b3816235738b194dab22d1700dfa54d7a3cad
Sighi-04 <filipposighinolfi@protonmail.com>
2021-08-10 23:24:35 +0200

    .

--- mainscript.js
+++ mainscript.js
@@ -15,27 +15,48 @@ async function getReposInfo(){
 async function getCommitsInfo(proj1, proj2){
     //Defines the 2 projects as objects, which will be used to change the html
     project1 = {
-        "name": proj1.name,
-        "description": proj1.description,
-        "committedAt": null,
-        "commitMessage": null
+        "Name": proj1.name,
+        "Description": proj1.description,
+        "CommittedAt": null,
+        "CommitMessage": null
     }
     project2 = {
-        "name": proj2.name,
-        "description": proj2.description,
-        "committedAt": null,
-        "commitMessage": null
+        "Name": proj2.name,
+        "Description": proj2.description,
+        "CommittedAt": null,
+        "CommitMessage": null
     }
     try {
-        //Fetches the first projects
+        //Fetches the first project's commit info
         let url="https://api.github.com/repos/Sighi-04/"+proj1.name+"/commits/main"
         let response=await fetch(url)
-        console.log(url)
         let result = await response.json()
-        await console.log(result.commit.message)
+        project1.CommitMessage= await result.commit.message
+        project1.CommittedAt= await result.commit.author.date
+        
+        //Fetches the second project's commit info
+        url="https://api.github.com/repos/Sighi-04/"+proj2.name+"/commits/main"
+        response=await fetch(url)
+        result = await response.json()
+        project2.CommitMessage= await result.commit.message
+        project2.CommittedAt= await result.commit.author.date
+
+        changeData(project1, project2)
+        
     }
     catch {
         alert("Impossibile ottenere i dati sui commit")
     }
 
+}
+
+function changeData(proj1, proj2) {
+    projects = ["proj1", "proj2"]
+    details = ["Name", "Desc", "CommitMessage", "CommittedAt"]
+    projects.forEach(project => {
+        details.forEach(detail => {
+            let temp = document.getElementById(project+detail)
+            temp.innerHTML = project1.detail
+        })
+    });
 }
